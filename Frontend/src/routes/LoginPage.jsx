630bcb20f2ee9fe3f483556f13d17776b8ede09c
balamurugan-s-dev <balabala13570@gmail.com>
2026-01-07 23:41:00 +0530

    login page prev final

--- Frontend/src/routes/LoginPage.jsx
+++ Frontend/src/routes/LoginPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Logo from '../assets/logo';
+import GoogleIcon from '../components/icons/GoogleIcon';
 
 const LoginPage = () => {
   return (
@@ -12,21 +13,21 @@ const LoginPage = () => {
           <form className="flex flex-col gap-6">
             <div className="flex flex-col gap-1">
               <label htmlFor="" className="font-medium">Email</label>
-              <input className="border-2 border-gray-400 rounded-md px-2 py-2 text-sm" type="email" placeholder='Enter your email'/>
+              <input className="border-2 border-gray-400 rounded-md px-3 py-3 text-sm" type="email" placeholder='Enter your email'/>
             </div>
             <div className="flex flex-col gap-1">
               <label htmlFor="" className="font-medium">Password</label>
-              <input className="border-2 border-gray-400 rounded-md px-2 py-2 text-sm" type="password" placeholder='Enter your password' />
+              <input className="border-2 border-gray-400 rounded-md px-3 py-3 text-sm" type="password" placeholder='Enter your password' />
             </div>
           </form>
           <p className=" text-sm font-bold cursor-pointer">Forget password</p>
 
           <div className='flex flex-col gap-4'>
-            <button className="bg-[#021526] rounded-md px-2 py-2 text-white text-sm font-semibold cursor-pointer">Login</button>
-            <button className="border-2 border-gray-400 rounded-md px-2 py-2 text-sm text-gray-900 font-semibold cursor-pointer">Sign in witn Google</button>
+            <button className="bg-[#021526] rounded-md px-3 py-3 text-white text-sm font-semibold cursor-pointer">Login</button>
+            <button className="border-2 border-gray-400 rounded-md px-2 py-2 text-sm text-[#021526] font-semibold cursor-pointer flex items-center justify-center gap-2"> <GoogleIcon w={25} h={25}/> <span>Sign in witn Google</span></button>
           </div>
 
-          <p className="text-sm my-4">Don't have an account?<span className="text-gray-950 font-semibold cursor-pointer"> Sign up for free</span></p>
+          <p className="text-sm my-4 text-center">Don't have an account?<span className="text-[#021526] font-semibold cursor-pointer"> Sign up for free</span></p>
         </div>
       </div>
     </div>
