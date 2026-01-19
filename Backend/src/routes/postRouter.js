b5706b4c20579afdf84fc2448810232cf8ea3cf0
balamurugan-s-dev <balabala13570@gmail.com>
2026-01-19 22:34:34 +0530

    postrouter update

--- Backend/src/routes/postRouter.js
+++ Backend/src/routes/postRouter.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getPost, getPosts } from '../controllers/postController.js';
+import { createPost, getPost, getPosts } from '../controllers/postController.js';
 
 const postRouter = express.Router();
 
 postRouter.get('/post', getPosts)
 postRouter.get('/post/:slug', getPost)
+postRouter.post('/post/create', createPost)
 
 export default postRouter;
